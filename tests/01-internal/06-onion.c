273c14f2aa6ff41bba0e9c16c660e5b96c7b0cb0
David Moreno <monjemono@gmail.com>
2010-10-16 17:42:08 +0200

    Added initial onion test. It will test the normal api use.

---
+++ tests/01-internal/06-onion.c
@@ -0,0 +1,39 @@
+/*
+	Onion HTTP server library
+	Copyright (C) 2010 David Moreno Montero
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as
+	published by the Free Software Foundation, either version 3 of the
+	License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	*/
+
+#include <malloc.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <onion_dict.h>
+#include <onion_server.h>
+#include <onion_request.h>
+#include <onion_response.h>
+#include <onion_handler.h>
+#include <handlers/onion_handler_static.h>
+#include <handlers/onion_handler_path.h>
+
+#include "../test.h"
+
+
+
+int main(int argc, char **argv){
+	
+	END();
+}
+
