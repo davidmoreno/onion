f3407efb5da371824cbda36e8423b9419a3d399d
David Moreno <monjemono@gmail.com>
2010-10-27 20:45:03 +0200

    Added missing test 02-fullserver.

---
+++ tests/02-fullserver/01-userver.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#normal download
+
+../../examples/userver . &
+
+wget -O test http://localhost:8080/01-userver.sh
+DF=$( diff test 01-userver.sh )
+rm test
+if [ "$DF" ]; then
+	echo "Error: Download did not give the right file"
+	exit 1
+fi
+
+# get out of known places
+wget -O test http://localhost:8080/../../../../../../../etc/passwd
+DF=$( diff test ../../../../../../../etc/passwd )
+rm test
+if [ ! "$DF" ]; then
+	echo "Download gave the file! and its out of allowed scope!"
+	exit 1
+fi 
+
+
