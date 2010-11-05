aeb5dbf141d92faeb78d60387b2239086e540411
David Moreno <monjemono@gmail.com>
2010-11-05 10:18:14 +0100

    Added some command line parameters: help and port.

--- examples/oterm/oterm.c
+++ examples/oterm/oterm.c
@@ -32,6 +32,15 @@
 
 onion *o=NULL;
 
+/// Prints some help to stderr
+void show_help(){
+	fprintf(stderr," oterm - Linux terminal over HTTP\n\n"
+								 "It uses HTTP(S)+HTML+CSS+JavaScript to show a remote terminal inside a browser.\n\n"
+								 "Options:\n"
+								 "   -p|--port [port_number] -- Set the port number to use. Default 8080\n"
+								 "\n");
+}
+
 void free_onion(){
 	fprintf(stderr,"\nClosing connections.\n");
 	onion_free(o);
@@ -39,6 +48,25 @@ void free_onion(){
 }
 
 int main(int argc, char **argv){
+	int port=8080;
+	int i;
+	for (i=1;i<argc;i++){
+		if (strcmp(argv[i],"--help")==0){
+			show_help();
+			exit(0);
+		}
+		else if(strcmp(argv[i],"-p")==0 || strcmp(argv[i],"--port")==0){
+			if (i+1>argc){
+				fprintf(stderr, "Need to set the port number.\n");
+				show_help();
+				exit(1);
+			}
+			port=atoi(argv[++i]);
+			fprintf(stderr, "Using port %d\n",port);
+		}
+	}
+	
+	
 #ifdef __DEBUG__
 	onion_handler *dir=onion_handler_directory(".");
 #else
@@ -51,9 +79,6 @@ int main(int argc, char **argv){
 	
 	o=onion_new(O_ONE);
 	onion_set_root_handler(o, dir);
-	int port=8080;
-	if (getenv("ONION_PORT"))
-		port=atoi(getenv("ONION_PORT"));
 	
 	onion_set_port(o, port);
 	
