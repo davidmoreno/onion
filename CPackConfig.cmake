a7af8a7629f0b25da9fc5c8e1a274e1765fd730c
David Moreno <dmoreno@coralbits.com>
2010-12-31 16:09:06 +0000

    Added missing CPackConfig file to help package constructions.

---
+++ CPackConfig.cmake
@@ -0,0 +1,14 @@
+SET(CPACK_PACKAGE_NAME libonion)
+SET(CPACK_PACKAGE_VERSION ${ONION_VERSION})
+SET(CPACK_DEBIAN_PACKAGE_DEPENDS "libgnutls26, libpam0g")
+
+SET(CPACK_GENERATOR "STGZ;TGZ;DEB")
+
+SET(CPACK_PACKAGE_CONTACT "David Moreno Montero <dmoreno@coralbits.com>")
+SET(CPACK_PACKAGE_DESCRIPTION_SUMMARY "HTTP server library to create or embbed easily a web server")
+
+SET(CPACK_RESOURCE_FILE_README ${CMAKE_SOURCE_DIR}/README.rst)
+
+MESSAGE(STATUS "Prepared for packaging")
+ 
+
