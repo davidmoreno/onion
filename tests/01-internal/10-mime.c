e591e939ef6ac77725ea7600139a59a580b38c91
David Moreno <dmoreno@coralbits.com>
2011-05-01 11:07:19 +0200

    Added missing mime.c test.

---
+++ tests/01-internal/10-mime.c
@@ -0,0 +1,41 @@
+/*
+	Onion HTTP server library
+	Copyright (C) 2010 David Moreno Montero
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as
+	published by the Free Software Foundation, either version 3 of the
+	License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	*/
+
+#include <onion/mime.h>
+#include "../test.h"
+
+void t01_test_mime(){
+	INIT_LOCAL();
+	
+	FAIL_IF_NOT_EQUAL_STR("text/plain", onion_mime_get("txt"));
+	FAIL_IF_NOT_EQUAL_STR("text/plain", onion_mime_get("fdsfds"));
+	FAIL_IF_NOT_EQUAL_STR("text/html", onion_mime_get("html"));
+	FAIL_IF_NOT_EQUAL_STR("text/x-csrc", onion_mime_get("c"));
+	FAIL_IF_NOT_EQUAL_STR("application/javascript", onion_mime_get("js"));
+
+	
+	onion_mime_set(NULL);
+	END_LOCAL();
+}
+
+
+int main(int argc, char **argv){
+	t01_test_mime();
+	
+	END();
+}
