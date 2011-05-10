24d908f89d41e573f079da610ec5e4480ebafa56
David Moreno <dmoreno@coralbits.com>
2011-05-10 18:05:16 +0100

    Small fix to hello example to use better url_static than the static_handler.

--- examples/hello/hello.c
+++ examples/hello/hello.c
@@ -1,6 +1,5 @@
 /** Licensed under AGPL 3.0. (C) 2010 David Moreno Montero. http://coralbits.com */
 #include <onion/onion.h>
-#include <onion/handlers/static.h>
 
 int hello(void *p, onion_request *req, onion_response *res){
 	onion_response_set_length(res, 11);
@@ -12,7 +11,7 @@ int main(int argc, char **argv){
 	onion *o=onion_new(O_THREADED);
 	onion_url *urls=onion_root_url(o);
 	
-	onion_url_add_handler(urls, "static", onion_handler_static("Hello static world", HTTP_OK));
+	onion_url_add_static(urls, "^static$", "Hello static world", HTTP_OK);
 	onion_url_add(urls, "^$", hello);
 	
 	onion_listen(o);
