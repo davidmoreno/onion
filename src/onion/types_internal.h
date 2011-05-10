eb9f5b97cffff40206c053c1f2bd3103bcc63f71
David Moreno <dmoreno@coralbits.com>
2011-05-10 12:41:34 +0100

    Some comment updates.

--- src/onion/types_internal.h
+++ src/onion/types_internal.h
@@ -113,11 +113,11 @@ struct onion_response_t{
 };
 
 struct onion_handler_t{
-	onion_handler_handler handler;  /// callback that should return an onion_response object, or NULL if im not entitled to respnse this request.
+	onion_handler_handler handler;  /// callback that should return an onion_connection_status, and maybe process the request.
 	onion_handler_private_data_free priv_data_free;  /// When freeing some memory, how to remove the private memory.
-	void *priv_data;                /// Private data as needed by the parser
+	void *priv_data;                /// Private data as needed by the handler
 	
-	struct onion_handler_t *next; /// If parser returns null, i try next parser. If no next parser i go up, or return an error. @see onion_tree_parser
+	struct onion_handler_t *next; /// If parser returns null, i try next handler. If no next handler i go up, or return an error. @see onion_handler_handle
 };
 
 struct onion_sessions_t{
