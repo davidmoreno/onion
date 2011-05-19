1557c4e0913426649d4ed8b6c287ad24af450de9
David Moreno <dmoreno@coralbits.com>
2011-05-19 20:12:18 +0100

    Added auto.sh to automatically run all internal tests.

---
+++ tests/auto.sh
@@ -0,0 +1,17 @@
+cd 01-internal
+FAILED=""
+
+for i in $( find -maxdepth 1 -type f -executable ); do
+	$i
+	if [ "$?" != "0" ]; then
+		FAILED="$FAILED $i"
+	fi
+done
+
+if [ "$FAILED" ]; then
+	echo -e "\033[31mFAILED TESTS: $FAILED\033[0m"
+	exit 1
+else
+	echo "OK"
+	exit 0
+fi
