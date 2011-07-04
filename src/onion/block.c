9b3ae109e981a6c3a25ef8984bfc12c5fc269a52
David Moreno <dmoreno@coralbits.com>
2011-07-04 21:12:16 +0100

    License changed for the library to LGPL. Examples and tests remain AGPL.

--- src/onion/block.c
+++ src/onion/block.c
@@ -2,18 +2,18 @@
 	Onion HTTP server library
 	Copyright (C) 2010-2011 David Moreno Montero
 
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU Affero General Public License as
-	published by the Free Software Foundation, either version 3 of the
-	License, or (at your option) any later version.
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Lesser General Public
+	License as published by the Free Software Foundation; either
+	version 3.0 of the License, or (at your option) any later version.
 
-	This program is distributed in the hope that it will be useful,
+	This library is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU Affero General Public License for more details.
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+	Lesser General Public License for more details.
 
-	You should have received a copy of the GNU Affero General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	You should have received a copy of the GNU Lesser General Public
+	License along with this library; if not see <http://www.gnu.org/licenses/>.
 	*/
 
 #include "types_internal.h"
