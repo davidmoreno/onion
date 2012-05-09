4a3559179ebb55d1595563800d1761a278b31c62
David Moreno <dmoreno@coralbits.com>
2012-05-09 12:09:51 +0100

    Added websocket.c/websocket.h with basic websocket support. Also a simple echo example.

---
+++ src/onion/websocket.h
@@ -0,0 +1,45 @@
+/*
+	Onion HTTP server library
+	Copyright (C) 2011-2012 David Moreno Montero
+
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Lesser General Public
+	License as published by the Free Software Foundation; either
+	version 3.0 of the License, or (at your option) any later version.
+
+	This library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+	Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public
+	License along with this library; if not see <http://www.gnu.org/licenses/>.
+	*/
+
+#ifndef __WEBSOCKETS_H__
+#define __WEBSOCKETS_H__
+
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+#include <stddef.h>
+#include "types.h"
+
+/// Get the current websocket handler, or create it. If not a websocket request, returns NULL
+onion_websocket *onion_websocket_new(onion_request *req, onion_response *res);
+void onion_websocket_free(onion_websocket *ws);
+
+void onion_websocket_set_callback(onion_websocket *ws, onion_websocket_callback_t cb);
+int onion_websocket_read(onion_websocket *ws, char *buffer, size_t len);
+int onion_websocket_write(onion_websocket *ws, const char *buffer, size_t len);
+int onion_websocket_printf(onion_websocket *ws, const char *str, ...);
+onion_connection_status onion_websocket_call(onion_websocket *ws);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
