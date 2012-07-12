1c5e96f2a88f84647f7013406bc8881e2db7d1bf
David Moreno <dmoreno@coralbits.com>
2012-07-12 10:46:50 +0100

    Added in mem buffer listen point for use in tests.

---
+++ tests/01-internal/buffer_listen_point.c
@@ -0,0 +1,60 @@
+/*
+	Onion HTTP server library
+	Copyright (C) 2010 David Moreno Montero
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as
+	published by the Free Software Foundation, either version 3 of the
+	License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	*/
+
+#include <malloc.h>
+
+#include <onion/log.h>
+#include <onion/http.h>
+#include <onion/types.h>
+#include <onion/types_internal.h>
+#include <onion/request.h>
+
+
+static void oblp_onion_request_close(onion_request *lp){
+	free(lp->connection.user_data);
+	onion_request_free(lp);
+}
+
+static ssize_t oblp_write_append(onion_request *a, const char *b, size_t size){
+	ONION_DEBUG("Write %d bytes",size);
+	return size;
+}
+
+static void oblp_listen(onion_listen_point *lp){
+	ONION_DEBUG("Empty listen for buffer listen point.");
+	return;
+}
+
+static void oblp_onion_request_init(onion_request *req){
+	ONION_DEBUG("Empty init.");
+}
+
+const char* onion_buffer_listen_point_get_buffer_data(onion_request* req)
+{
+		return req->connection.user_data;
+}
+
+onion_listen_point* onion_buffer_listen_point_new()
+{
+	onion_listen_point *lp=onion_http_new();
+	lp->request_init=oblp_onion_request_init;
+	lp->write=oblp_write_append;
+	lp->close=oblp_onion_request_close;
+	lp->listen=oblp_listen;
+	return lp;
+}
