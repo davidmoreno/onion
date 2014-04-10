davidmoreno/onion

commit 2d2e16a871dffbc71782b55e0bc7da728bc12e00

Added dict to map operator.

tests/08-cpp/02-dict.cpp
@@ -167,6 +167,19 @@ void t05_context(){
 }
 
 
+void t06_tomap(){
+	INIT_LOCAL();
+	
+	std::map<std::string, std::string> orig{{"Hello","World"}};
+	Onion::Dict d(orig);
+	
+	std::map<std::string, std::string> dup=d;
+	
+	FAIL_IF_NOT_EQUAL_STRING(dup["Hello"],"World");
+	
+	END_LOCAL();
+}
+
 int main(int argc, char **argv){
   START();
 	INFO("Remember to check with valgrind");
@@ -175,6 +188,7 @@ int main(int argc, char **argv){
 	t03_subdict();
 	t04_langtest();
 	t05_context();
+	t06_tomap();
 	
 	END();
 }

src/bindings/cpp/CMakeLists.txt
@@ -1,6 +1,6 @@
 include_directories(${PROJECT_SOURCE_DIR}/src/)
-add_library(onioncpp SHARED handler.cpp extrahandlers.cpp)
-add_library(onioncpp_static STATIC handler.cpp extrahandlers.cpp)
+add_library(onioncpp SHARED dict.cpp handler.cpp extrahandlers.cpp)
+add_library(onioncpp_static STATIC dict.cpp handler.cpp extrahandlers.cpp)
 
 SET(INCLUDES_ONIONCPP onion.hpp dict.hpp request.hpp response.hpp url.hpp handler.hpp extrahandlers.hpp)
 MESSAGE(STATUS "Found include files ${INCLUDES_ONIONCPP}")

src/bindings/cpp/dict.cpp
@@ -1,0 +1,39 @@
+/*
+	Onion HTTP server library
+	Copyright (C) 2010-2014 David Moreno Montero and othes
+
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of, at your choice:
+	
+	a. the Apache License Version 2.0. 
+	
+	b. the GNU General Public License as published by the 
+		Free Software Foundation; either version 2.0 of the License, 
+		or (at your option) any later version.
+	 
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of both libraries, if not see 
+	<http://www.gnu.org/licenses/> and 
+	<http://www.apache.org/licenses/LICENSE-2.0>.
+	*/
+
+#include "dict.hpp"
+#include <map>
+
+namespace Onion{
+	void add_to_map(std::map<std::string, std::string> *ret, const char *key, const char *value){
+		(*ret)[key]=value;
+	}
+	
+	Dict::operator std::map<std::string, std::string>(){
+		std::map<std::string, std::string> ret;
+		
+		onion_dict_preorder(c_handler(), (void*)add_to_map, (void*)&ret);
+		
+		return ret;
+	}
+};

src/bindings/cpp/dict.hpp
@@ -135,6 +135,8 @@ namespace Onion{
 			return str;
 		}
 		
+		operator std::map<std::string, std::string>();
+		
 		onion_dict *c_handler(){
 			return ptr;
 		}

commit 0db9b7ef18c93d46d993b2636cf2de045a0cfa82

C++ fixes for old gcc (SLE_11, RHEL-6)
was broken since 2d2e16a8

tests/08-cpp/02-dict.cpp
@@ -187,7 +187,12 @@ void t05_context(){
 void t06_tomap(){
 	INIT_LOCAL();
 	
+#if __cplusplus > 199711L
 	std::map<std::string, std::string> orig{{"Hello","World"}};
+#else
+	std::map<std::string, std::string> orig;
+	orig.insert(std::pair< std::string, std::string>("Hello","World"));
+#endif
 	Onion::Dict d(orig);
 	
 	std::map<std::string, std::string> dup=d;