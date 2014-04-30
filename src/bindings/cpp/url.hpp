0abf7f3c72d1a09509e262405fef68cb031d6364
Sergey Kovalevich <kovalevich@qfcap.ru>
2014-04-30 18:02:21 +0400

    fixed mistype

--- src/bindings/cpp/url.hpp
+++ src/bindings/cpp/url.hpp
@@ -4,20 +4,20 @@
 
 	This library is free software; you can redistribute it and/or
 	modify it under the terms of, at your choice:
-	
-	a. the Apache License Version 2.0. 
-	
-	b. the GNU General Public License as published by the 
-		Free Software Foundation; either version 2.0 of the License, 
+
+	a. the Apache License Version 2.0.
+
+	b. the GNU General Public License as published by the
+		Free Software Foundation; either version 2.0 of the License,
 		or (at your option) any later version.
-	
+
 	This program is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 	GNU General Public License for more details.
 
-	You should have received a copy of both libraries, if not see 
-	<http://www.gnu.org/licenses/> and 
+	You should have received a copy of both libraries, if not see
+	<http://www.gnu.org/licenses/> and
 	<http://www.apache.org/licenses/LICENSE-2.0>.
 	*/
 
@@ -32,37 +32,37 @@
 namespace Onion{
 	/**
 	 * @short Url management for Onion
-	 * 
+	 *
 	 * With this class its possible to create the muxer necesary to redirect the petitions to the proper handler.
-	 * 
+	 *
 	 * Normally user writes some regex* to map from the path to a request handler. It follows Django trail and urls can
-	 * be nested, so a regex matches the start of an url, and the next handler the next part of the url. It also allows 
+	 * be nested, so a regex matches the start of an url, and the next handler the next part of the url. It also allows
 	 * groups as in django, but only anonymous groups.
-	 * 
+	 *
 	 * These groups will be set at the query as fields 1, 2, 3... as found.
-	 * 
+	 *
 	 * Example:
-	 * 
+	 *
 	 * \code
 	 *   Onion::Url url;
 	 *   Onion::Url more_urls;
-	 * 
+	 *
 	 *   url.add("", std::string("Index")); // Returns static data, just the word Index.
 	 *   url.add("^more", more_urls);
-	 * 
+	 *
 	 *   more_urls.add("", std::string("More")); // At /more/ returns "More"
 	 *   more_urls.add("more", std::string("Even more")); // At /more/more/ returns "Even more".
 	 * \endcode
-	 * 
+	 *
 	 * Urls may be regex or plain strings. If plain strings it tries to match the full string. This is much faster, but
 	 * limits the subURLing. If using a regex its possible to create suburls, just matching the string start: "^more". Internally
 	 * this is as fast as the full string match, as it really dont use regex. So, in order of preference, use this kind of patterns:
-	 * 
+	 *
 	 * * "^startswith"
 	 * * "fullmatch"
-	 * * "^regex_full_match$" 
+	 * * "^regex_full_match$"
 	 * * "^regex_(group|match)$"
-	 * 
+	 *
 	 * As it can se regex without full matching, be careful or its possible to just match substrings: "o$" matches "Hello", "Hello/World/o" and so on.
 	 */
 	class Url{
@@ -93,7 +93,7 @@ namespace Onion{
 		 * @short Returns the C handler to use onion_url C functions.
 		 */
 		onion_url *c_handler(){ return ptr;  }
-		
+
 		/**
 		 * @short Adds an url that calls an Onion::Handler-
 		 */
@@ -108,9 +108,9 @@ namespace Onion{
 		}
 		/**
 		 * @short Adds an url that calls a C++ function.
-		 * 
+		 *
 		 * Example:
-		 * 
+		 *
 		 *   url.add("", [](Onion::Request &req, Onion::Response &res){ return OCS_INTERNAL_ERROR; });
 		 */
 		bool add(const std::string &url, HandlerFunction::fn_t fn){
@@ -118,19 +118,19 @@ namespace Onion{
 		}
 		/**
 		 * @short Adds an url that calls a C++ method.
-		 * 
+		 *
 		 * This is the most normal way to add several calls to methods on the same class.
-		 * 
+		 *
 		 * Example:
-		 * 
+		 *
 		 * \code
 		 *   class MyClass{
 		 *    public:
-		 *     onion_connection_status index(Onion::Request &res, Onion::Response &res);
+		 *     onion_connection_status index(Onion::Request &req, Onion::Response &res);
 		 *   };
-		 * 
+		 *
 		 *   ...
-		 * 
+		 *
 		 *   Onion::Url url;
 		 *   MyClass c;
 		 *   url.add("mycall", &c, MyClass::index);
@@ -148,7 +148,7 @@ namespace Onion{
 		}
 		/**
 		 * @short Adds an url that calls a C style onion handler.
-		 * 
+		 *
 		 * With this method is possible to use the C handlers as onion_webdav.
 		 */
 		bool add(const std::string &url, onion_handler_handler handler){
@@ -156,17 +156,17 @@ namespace Onion{
 		}
 		/**
 		 * @short Adds an url that calls another Onion::Url.
-		 * 
+		 *
 		 * With this method its possible to create Onion::Url hierachies, easing the modularization of
 		 * web applications.
 		 */
 		bool add(const std::string &url, Url &url_handler){
 			return add(url, onion_url_to_handler(url_handler.c_handler()));
 		}
-		
+
 		/**
 		 * @short Allows to call am Onion::Url to continue the processing of this request.
-		 * 
+		 *
 		 * A use case is a have a handler that controls access by means of a session (or CSRF control), and
 		 * if the conditions are OK, call the url handler to continue with the processing.
 		 */
