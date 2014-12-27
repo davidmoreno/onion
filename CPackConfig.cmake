5d04aae65570f20fafeadc742bf09d3dff320915
David Moreno <dmoreno@coralbits.com>
2014-12-27 16:00:55 +0100

    Fixed build, changed README.rst to README.md at cpack.

--- CPackConfig.cmake
+++ CPackConfig.cmake
@@ -7,7 +7,7 @@ SET(CPACK_GENERATOR "STGZ;TGZ;DEB")
 SET(CPACK_PACKAGE_CONTACT "David Moreno Montero <dmoreno@coralbits.com>")
 SET(CPACK_PACKAGE_DESCRIPTION_SUMMARY "HTTP server library to create or embbed easily a web server")
 
-SET(CPACK_RESOURCE_FILE_README ${PROJECT_SOURCE_DIR}/README.rst)
+SET(CPACK_RESOURCE_FILE_README ${PROJECT_SOURCE_DIR}/README.md)
 
 MESSAGE(STATUS "Prepared for packaging")
  
