b1e6afea0f9a090b9a22435abe7c0005fb1fa448
Zachary Grafton <zachary.grafton@gmail.com>
2015-12-25 22:41:12 -0500

    Make sure the strings are passed to redis safely.
    According to http://redis.io/topics/data-types all Redis strings are
    considering binary safe. In this case, I changed the data types passed
    to the hiredis library to be considered binary strings instead of string
    that may possibly be considered unsafe.

--- src/onion/sessions_redis.c
+++ src/onion/sessions_redis.c
@@ -71,7 +71,7 @@ static onion_dict* onion_sessions_redis_get(onion_sessions* sessions, const char
         // When commands are sent via redisCommand, they are interpolated by the library
         // so it will avoid any type of command injection. No need to worry about sending
         // the session_id directly to redis.
-	redisReply* reply = redisCommand(p->context, "HEXISTS SESSIONS %s", session_id);
+	redisReply* reply = redisCommand(p->context, "HEXISTS SESSIONS %b", session_id);
 
 	if(reply->type != REDIS_REPLY_INTEGER)
 	{
@@ -84,7 +84,7 @@ static onion_dict* onion_sessions_redis_get(onion_sessions* sessions, const char
 		if(reply->integer == 1)
 		{
 			freeReplyObject(reply);
-			reply = redisCommand(p->context, "HGET SESSIONS %s", session_id);
+			reply = redisCommand(p->context, "HGET SESSIONS %b", session_id);
 
 			if(reply->type != REDIS_REPLY_STRING)
 			{
@@ -126,7 +126,7 @@ void onion_sessions_redis_save(onion_sessions *sessions, const char *session_id,
 
 	if(p == NULL)
 	{
-		redisReply* reply = redisCommand(p->context, "HDEL SESSIONS %S", session_id);
+		redisReply* reply = redisCommand(p->context, "HDEL SESSIONS %b", session_id);
 
 		if(reply->type != REDIS_REPLY_INTEGER)
 		{
@@ -136,7 +136,7 @@ void onion_sessions_redis_save(onion_sessions *sessions, const char *session_id,
 	} else 
     {
 		const char* json = onion_block_data(bl);
-		redisReply* reply = redisCommand(p->context, "HSET SESSIONS %s %s", session_id, json);
+		redisReply* reply = redisCommand(p->context, "HSET SESSIONS %b %b", session_id, json);
 
 		if(reply->type != REDIS_REPLY_INTEGER)
 		{
