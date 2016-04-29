7314ed7d823d25c4587ff82a3ea4f4a37860f4d5
David Moreno <dmoreno@coralbits.com>
2016-04-29 11:07:16 +0200

    Updated copyright date and "and othes" typo.

--- src/onion/extras/jpeg.c
+++ src/onion/extras/jpeg.c
@@ -1,6 +1,6 @@
 /*
 	 Onion HTTP server library
-	 Copyright (C) 2010-2014 David Moreno Montero and othes
+	 Copyright (C) 2010-2016 David Moreno Montero and others
 
 	 This library is free software; you can redistribute it and/or
 	 modify it under the terms of, at your choice:
