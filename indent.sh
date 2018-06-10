85c1d9347566b4b45c51de0a6ea9b97fac2dbb96
David Moreno <dmoreno@coralbits.com>
2018-06-10 10:49:32 +0200

    Added indent make rule
    Removed the indent script as the rule does it all.

--- indent.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-INDENT=indent
-OPTIONS="-linux -i2 -nut -brf"
-FILES=$( find src tools tests examples -name "*.c" -or -name "*.h" )
-
-echo "Indenting files..."
-$INDENT $OPTIONS $FILES
-echo "Done"
