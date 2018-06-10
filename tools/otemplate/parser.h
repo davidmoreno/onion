24f91ec96ccf636fe1358ffff4fab995a9d2cf2f
Basile Starynkevitch <basile@starynkevitch.net>
2018-06-10 09:27:55 +0200

    Add ability to skip and parse with {# template comments #}     Fix issue #224
        The source code of parser.c & parser.h has been indented in GNU style with indent -gnu

--- tools/otemplate/parser.h
+++ tools/otemplate/parser.h
@@ -1,6 +1,6 @@
 /*
 	Onion HTTP server library
-	Copyright (C) 2010-2011 David Moreno Montero
+	Copyright (C) 2010-2018 David Moreno Montero and contributors
 
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU Affero General Public License as
@@ -26,45 +26,51 @@
 #include "functions.h"
 #include "list.h"
 
-typedef enum parser_mode_e{
-	TEXT=0,
-	VARIABLE=2,
-	TAG=3,
-	
-	END=15,
-	// transitional modes
-	BEGIN=17,
-	END_VARIABLE=18,
-	END_TAG=19,
-	
-}parser_mode;
+typedef enum parser_mode_e
+{
+  TEXT = 0,
+  VARIABLE = 2,
+  TAG = 3,
+
+  END = 15,
+
+  TRANSITIONAL_MODE_THRESHOLD = 16,
+  // transitional modes
+  BEGIN = 17,
+  END_VARIABLE = 18,
+  END_TAG = 19,
+  COMMENT = 20,			// inside comments
+  COMMENTHASH = 21,		// got an hash in a comment
+
+} parser_mode;
 
 
-struct parser_status_t{
-	parser_mode last_wmode;
-	parser_mode mode;
-	FILE *in;
-	FILE *out;
-	
-	const char *infilename; /// Needed for includes using relative path
-	
-	onion_block *rawblock; /// Current block of data as readed by the parser.
-	
-	list *functions; /// List of all known functions
-	list *function_stack; /// Current stack of functions, to know where to write.
-	function_data *blocks_init; /// List of block names
-	onion_block *current_code; /// Blocks of C code to be printed. this is final code.
-	char c; /// Last character read
-	int status; /// Exit status.
-	int function_count;
-	int line;
+struct parser_status_t
+{
+  parser_mode last_wmode;
+  parser_mode mode;
+  FILE *in;
+  FILE *out;
+
+  const char *infilename;	/// Needed for includes using relative path
+
+  onion_block *rawblock;	/// Current block of data as readed by the parser.
+
+  list *functions;		/// List of all known functions
+  list *function_stack;		/// Current stack of functions, to know where to write.
+  function_data *blocks_init;	/// List of block names
+  onion_block *current_code;	/// Blocks of C code to be printed. this is final code.
+  char c;			/// Last character read
+  int status;			/// Exit status.
+  int function_count;
+  int line;
 };
 typedef struct parser_status_t parser_status;
 
-void parse_template(parser_status *status);
+void parse_template (parser_status * status);
 
-void add_char(parser_status *st, char c);
-void write_block(parser_status *st, onion_block *b);
+void add_char (parser_status * st, char c);
+void write_block (parser_status * st, onion_block * b);
 
 
 #endif
